3d7162cd889c543e9dc9aff8ed394dd6d42a780c
Franco <lorcafrancog@gmail.com>
2021-12-17 14:12:14 -0300

    Added datasource
    Changed application.properties to application.yml and added datasource configuration

---
+++ src/test/java/com/challenge/disney/DisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
