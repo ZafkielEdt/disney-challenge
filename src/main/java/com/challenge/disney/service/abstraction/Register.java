1fb7dff0bb1146632e656c3c53bd906f812cf284
Franco Lorca <lorcafrancog@gmail.com>
2023-01-21 16:22:46 -0300

    Update
    Added operation for get and set role to new user

--- src/main/java/com/challenge/disney/service/abstraction/Register.java
+++ src/main/java/com/challenge/disney/service/abstraction/Register.java
@@ -4,9 +4,10 @@ import com.challenge.disney.exception.EmailAlreadyExistsException;
 import com.challenge.disney.exception.UsernameAlreadyExistsException;
 import com.challenge.disney.model.request.RegisterRequest;
 import com.challenge.disney.model.response.RegisterResponse;
+import javax.management.relation.RoleNotFoundException;
 
 public interface Register {
 
   RegisterResponse register(RegisterRequest request)
-      throws EmailAlreadyExistsException, UsernameAlreadyExistsException;
+      throws EmailAlreadyExistsException, UsernameAlreadyExistsException, RoleNotFoundException;
 }
--- src/main/java/com/challenge/disney/service/auth/RegisterImpl.java
+++ src/main/java/com/challenge/disney/service/auth/RegisterImpl.java
@@ -1,13 +1,18 @@
 package com.challenge.disney.service.auth;
 
+import com.challenge.disney.config.security.RoleType;
 import com.challenge.disney.exception.EmailAlreadyExistsException;
 import com.challenge.disney.exception.UsernameAlreadyExistsException;
 import com.challenge.disney.mapper.UserMapper;
+import com.challenge.disney.model.entity.Role;
 import com.challenge.disney.model.entity.User;
 import com.challenge.disney.model.request.RegisterRequest;
 import com.challenge.disney.model.response.RegisterResponse;
+import com.challenge.disney.repository.RoleRepository;
 import com.challenge.disney.repository.UserRepository;
 import com.challenge.disney.service.abstraction.Register;
+import java.util.Set;
+import javax.management.relation.RoleNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,9 +27,11 @@ public class RegisterImpl implements Register {
 
   private final BCryptPasswordEncoder encoder;
 
+  private final RoleRepository roleRepository;
+
   @Override
   public RegisterResponse register(RegisterRequest request)
-      throws EmailAlreadyExistsException, UsernameAlreadyExistsException {
+      throws EmailAlreadyExistsException, UsernameAlreadyExistsException, RoleNotFoundException {
 
     if (userRepository.existsByEmail(request.getEmail())) {
       throw new EmailAlreadyExistsException("Email already exists");
@@ -34,8 +41,12 @@ public class RegisterImpl implements Register {
       throw new UsernameAlreadyExistsException("Username already exists");
     }
 
+    Role role = roleRepository.findByName(RoleType.USER.getFullRoleName())
+        .orElseThrow(() -> new RoleNotFoundException("Role not found"));
+
     User user = userMapper.map(request);
     user.setPassword(encoder.encode(user.getPassword()));
+    user.setRoles(Set.of(role));
     user.setSoftDelete(false);
 
     return userMapper.map(userRepository.save(user));
