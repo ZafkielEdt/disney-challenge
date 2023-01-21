8dfc9aed3b374fed040570aad72c9d45bbda7f16
Franco Lorca <lorcafrancog@gmail.com>
2023-01-21 11:26:50 -0300

    Create
    Created role repository

---
+++ src/main/java/com/challenge/disney/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.challenge.disney.repository;
+
+import com.challenge.disney.model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
