ea0a7ebef78b5ee0ade89a5d7d31ee367e2b8b8c
Franco Lorca <lorcafrancog@gmail.com>
2023-01-21 16:47:42 -0300

    Create
    Created implementation for UserDetailsService

---
+++ src/main/java/com/challenge/disney/service/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.challenge.disney.service.auth;
+
+import com.challenge.disney.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return userRepository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+}
