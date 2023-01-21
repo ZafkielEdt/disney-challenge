fee67cb9d1e7b3a5908952cd7ed44d7b42cd2862
Franco Lorca <lorcafrancog@gmail.com>
2023-01-21 16:22:06 -0300

    Update
    Added new query for get role

--- src/main/java/com/challenge/disney/repository/RoleRepository.java
+++ src/main/java/com/challenge/disney/repository/RoleRepository.java
@@ -1,10 +1,12 @@
 package com.challenge.disney.repository;
 
 import com.challenge.disney.model.entity.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+  Optional<Role> findByName(String name);
 }
