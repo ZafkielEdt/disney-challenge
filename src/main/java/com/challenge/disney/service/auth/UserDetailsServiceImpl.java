6264fff1cddcdb4dfe6750c8bd6a0b02c41ebfb3
Franco Lorca <lorcafrancog@gmail.com>
2023-01-23 13:43:31 -0300

    Update
    Updated service

--- src/main/java/com/challenge/disney/service/auth/UserDetailsServiceImpl.java
+++ src/main/java/com/challenge/disney/service/auth/UserDetailsServiceImpl.java
@@ -14,8 +14,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
   private final UserRepository userRepository;
 
   @Override
-  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    return userRepository.findByEmail(email)
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userRepository.findByUsername(username)
         .orElseThrow(() -> new UsernameNotFoundException("User not found"));
   }
 }
