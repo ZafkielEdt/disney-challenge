f55d88b9ea7afcf5c4d9b385c73b162a2be81de4
Franco Lorca <lorcafrancog@gmail.com>
2023-01-23 12:36:58 -0300

    Update
    Changed findByEmail to findByUsername

--- src/main/java/com/challenge/disney/repository/UserRepository.java
+++ src/main/java/com/challenge/disney/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  Optional<User> findByEmail(String email);
+  Optional<User> findByUsername(String username);
 
   Boolean existsByEmail(String email);
 
