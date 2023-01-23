810983dfffb31a916b5a8f570a3ee173c0f55cb1
Franco Lorca <lorcafrancog@gmail.com>
2023-01-23 14:41:04 -0300

    Update
    Updated paths

--- src/main/java/com/challenge/disney/config/security/SecurityFilter.java
+++ src/main/java/com/challenge/disney/config/security/SecurityFilter.java
@@ -2,6 +2,7 @@ package com.challenge.disney.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,10 @@ public class SecurityFilter {
         .csrf().disable()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
-        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        .authorizeHttpRequests(auth ->
+            auth.requestMatchers(HttpMethod.POST, "/api/v2/register").permitAll())
+        .authorizeHttpRequests(auth ->
+            auth.requestMatchers(HttpMethod.POST, "/api/v2/login").permitAll());
     return httpSecurity.build();
   }
 }
