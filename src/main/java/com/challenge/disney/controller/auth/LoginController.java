1c59c15410aae50e0c0cddec6e8a5435fa42f7ee
Franco Lorca <lorcafrancog@gmail.com>
2023-01-23 14:40:42 -0300

    Create
    Created LoginController

---
+++ src/main/java/com/challenge/disney/controller/auth/LoginController.java
@@ -0,0 +1,29 @@
+package com.challenge.disney.controller.auth;
+
+import com.challenge.disney.exception.InvalidCredentialsException;
+import com.challenge.disney.model.request.LoginRequest;
+import com.challenge.disney.model.response.LoginResponse;
+import com.challenge.disney.service.abstraction.Login;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/login")
+@RequiredArgsConstructor
+public class LoginController {
+
+  private final Login login;
+
+  @PostMapping
+  public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request)
+      throws InvalidCredentialsException {
+    LoginResponse response = login.login(request);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
+}
