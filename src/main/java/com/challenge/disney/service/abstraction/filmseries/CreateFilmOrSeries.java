e0be329c6acd9f6e89c8028630551c1bc6514f46
Franco Lorca <lorcafrancog@gmail.com>
2023-02-01 16:12:47 -0300

    feat: Added Exception

--- src/main/java/com/challenge/disney/service/abstraction/filmseries/CreateFilmOrSeries.java
+++ src/main/java/com/challenge/disney/service/abstraction/filmseries/CreateFilmOrSeries.java
@@ -1,9 +1,11 @@
 package com.challenge.disney.service.abstraction.filmseries;
 
+import com.challenge.disney.exception.FilmOrSeriesAlreadyExistsException;
 import com.challenge.disney.model.request.FilmOrSeriesRequest;
 import com.challenge.disney.model.response.FilmOrSeriesResponse;
 
 public interface CreateFilmOrSeries {
 
-  FilmOrSeriesResponse create(FilmOrSeriesRequest request);
+  FilmOrSeriesResponse create(FilmOrSeriesRequest request)
+      throws FilmOrSeriesAlreadyExistsException;
 }
