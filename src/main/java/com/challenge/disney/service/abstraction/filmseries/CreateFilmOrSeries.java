58f4132669691e46acb794fb1f4b1f88a093b197
Franco Lorca <lorcafrancog@gmail.com>
2023-02-01 15:27:29 -0300

    feat: Create interface CreateFilmOrSeries

---
+++ src/main/java/com/challenge/disney/service/abstraction/filmseries/CreateFilmOrSeries.java
@@ -0,0 +1,9 @@
+package com.challenge.disney.service.abstraction.filmseries;
+
+import com.challenge.disney.model.request.FilmOrSeriesRequest;
+import com.challenge.disney.model.response.FilmOrSeriesResponse;
+
+public interface CreateFilmOrSeries {
+
+  FilmOrSeriesResponse create(FilmOrSeriesRequest request);
+}
