f455931ea2ed473ed129909c3982a33275b9e9ea
Franco Lorca <lorcafrancog@gmail.com>
2023-02-01 16:13:10 -0300

    update: Updated CreateFilmOrSeriesImpl

--- src/main/java/com/challenge/disney/service/CreateFilmOrSeriesImpl.java
+++ src/main/java/com/challenge/disney/service/CreateFilmOrSeriesImpl.java
@@ -1,15 +1,33 @@
 package com.challenge.disney.service;
 
+import com.challenge.disney.exception.FilmOrSeriesAlreadyExistsException;
+import com.challenge.disney.mapper.FilmOrSeriesMapper;
+import com.challenge.disney.model.entity.FilmOrSeries;
 import com.challenge.disney.model.request.FilmOrSeriesRequest;
 import com.challenge.disney.model.response.FilmOrSeriesResponse;
+import com.challenge.disney.repository.FilmOrSeriesRepository;
 import com.challenge.disney.service.abstraction.filmseries.CreateFilmOrSeries;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CreateFilmOrSeriesImpl implements CreateFilmOrSeries {
 
+  private final FilmOrSeriesRepository filmOrSeriesRepository;
+
+  private final FilmOrSeriesMapper filmOrSeriesMapper;
+
   @Override
-  public FilmOrSeriesResponse create(FilmOrSeriesRequest request) {
-    return null;
+  public FilmOrSeriesResponse create(FilmOrSeriesRequest request)
+      throws FilmOrSeriesAlreadyExistsException {
+
+    if (filmOrSeriesRepository.existsByTitle(request.title())) {
+      throw new FilmOrSeriesAlreadyExistsException("Film or Series already exists");
+    }
+
+    FilmOrSeries filmOrSeries = filmOrSeriesMapper.map(request);
+
+    return filmOrSeriesMapper.map(filmOrSeriesRepository.save(filmOrSeries));
   }
 }
