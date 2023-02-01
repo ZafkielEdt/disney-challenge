618ce49b6b234c4125ed19bbbe2b455bf3e2b5ff
Franco Lorca <lorcafrancog@gmail.com>
2023-02-01 15:38:50 -0300

    feat: Create FilmOrSeriesRepository

---
+++ src/main/java/com/challenge/disney/repository/FilmOrSeriesRepository.java
@@ -0,0 +1,8 @@
+package com.challenge.disney.repository;
+
+import com.challenge.disney.model.entity.FilmOrSeries;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmOrSeriesRepository extends JpaRepository<FilmOrSeries, Long> {
+
+}
