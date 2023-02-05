4d9164b1103dd1341dc2757f341ab5736b6b60ce
Franco Lorca <lorcafrancog@gmail.com>
2023-02-05 13:22:37 -0300

    📦️Update: Added new method to RoleRepository

--- src/main/java/com/challenge/app/repository/RoleRepository.java
+++ src/main/java/com/challenge/app/repository/RoleRepository.java
@@ -1,8 +1,10 @@
 package com.challenge.app.repository;
 
 import com.challenge.app.model.entity.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+  Optional<Role> findByName(String name);
 }
