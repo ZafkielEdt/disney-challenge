d8cf86f5f2f0c0f32fd4405d3f62984bd5bca65e
Franco Lorca <lorcafrancog@gmail.com>
2023-02-05 13:13:18 -0300

    📦 Update: Add Role list propertie to User entity

--- src/main/java/com/challenge/app/model/entity/User.java
+++ src/main/java/com/challenge/app/model/entity/User.java
@@ -1,17 +1,23 @@
 package com.challenge.app.model.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.sql.Timestamp;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
@@ -34,6 +40,10 @@ public class User implements UserDetails {
   @Column(name = "PASSWORD", nullable = false)
   private String password;
 
+  @Column(name = "ROLE_ID", nullable = false)
+  @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+  private List<Role> roles;
+
   @CreationTimestamp
   @Column(name = "TIMESTAMP")
   private Timestamp timestamp;
@@ -43,7 +53,9 @@ public class User implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    return this.getRoles().stream()
+        .map(role -> new SimpleGrantedAuthority(role.getName()))
+        .collect(Collectors.toList());
   }
 
   @Override
