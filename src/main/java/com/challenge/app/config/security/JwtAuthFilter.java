d2f23e814a9b0cba5c1e7fa33bbb49c2cb913471
Franco Lorca <lorcafrancog@gmail.com>
2023-02-05 19:43:45 -0300

    ✨ Feat: Create ErrorResponseUtils

---
+++ src/main/java/com/challenge/app/config/security/JwtAuthFilter.java
@@ -0,0 +1,52 @@
+package com.challenge.app.config.security;
+
+
+import com.challenge.app.exception.InsufficientPermissionsException;
+import com.challenge.app.service.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+  private final UserDetailsServiceImpl userDetailsService;
+
+  private final JwtUtils jwtUtils;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    String token = jwtUtils.getToken(request);
+
+    if (!Objects.isNull(token)) {
+      try {
+        String username = jwtUtils.getUsernameFromToken(token);
+        if (!Objects.isNull(username) && jwtUtils.isValidToken(token)) {
+          UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+          UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+              userDetails.getUsername(), null, userDetails.getAuthorities());
+
+          SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+          throw new InsufficientPermissionsException("Access denied. Please, try to login again or contact your admin.");
+        }
+      } catch (Exception e) {
+        ErrorResponseUtils.setCustomResponse(response);
+      }
+    }
+
+    filterChain.doFilter(request, response);
+  }
+}
