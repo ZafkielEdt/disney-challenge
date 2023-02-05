041899077802c4bc365bc3153c00924f7b4341a7
Franco Lorca <lorcafrancog@gmail.com>
2023-02-05 18:56:13 -0300

    ✨ Feat: Create SecurityConfig

--- src/main/java/com/challenge/app/config/security/SecurityConfig.java
+++ src/main/java/com/challenge/app/config/security/SecurityConfig.java
@@ -1,17 +1,55 @@
 package com.challenge.app.config.security;
 
+import com.challenge.app.service.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+  private final UserDetailsImpl userDetails;
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+      AuthenticationManager authenticationManager)
+      throws Exception {
+    return httpSecurity.
+        csrf()
+        .disable()
+        .authorizeHttpRequests()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .build();
+  }
 
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder)
+      throws Exception {
+    return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+        .userDetailsService(userDetails)
+        .passwordEncoder(passwordEncoder)
+        .and().build();
   }
 }
