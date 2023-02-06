22173950e6469512ae5668f548a5435129794cd9
Franco Lorca <lorcafrancog@gmail.com>
2023-02-06 13:38:52 -0300

    📦️Update(UserRepository): Add new methods for check if exists the user

--- src/main/java/com/challenge/app/repository/UserRepository.java
+++ src/main/java/com/challenge/app/repository/UserRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
 }
