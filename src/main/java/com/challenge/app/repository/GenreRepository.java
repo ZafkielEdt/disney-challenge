b4d182870ffadf51aa63091247b026752081cafb
Franco Lorca <lorcafrancog@gmail.com>
2023-02-09 16:00:01 -0300

    ✨ Feat: Create new method

--- src/main/java/com/challenge/app/repository/GenreRepository.java
+++ src/main/java/com/challenge/app/repository/GenreRepository.java
@@ -1,9 +1,12 @@
 package com.challenge.app.repository;
 
 import com.challenge.app.model.entity.Genre;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+  List<Genre> findAll();
 
   Boolean existsByName(String name);
 }
