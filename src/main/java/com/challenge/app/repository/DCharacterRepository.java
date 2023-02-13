5d17df8d915238da49488d981554523cdeaf95c6
Franco Lorca <lorcafrancog@gmail.com>
2023-02-13 13:40:14 -0300

    ✨ Feat(DCharacterRepository): Add new query method to get by filters (name, age, filmSeriesId) with Specification and Pageable

--- src/main/java/com/challenge/app/repository/DCharacterRepository.java
+++ src/main/java/com/challenge/app/repository/DCharacterRepository.java
@@ -2,6 +2,9 @@ package com.challenge.app.repository;
 
 import com.challenge.app.model.entity.DCharacter;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DCharacterRepository extends JpaRepository<DCharacter, Long> {
@@ -9,4 +12,6 @@ public interface DCharacterRepository extends JpaRepository<DCharacter, Long> {
   Optional<DCharacter> findById(Long id);
 
   Boolean existsByName(String name);
+
+  Page<DCharacter> findAll(Specification<DCharacter> specification, Pageable pageable);
 }
