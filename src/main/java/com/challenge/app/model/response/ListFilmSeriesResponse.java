5cfa6262da4d1daa47bb24b5de0ea2d08e68ab72
Franco Lorca <lorcafrancog@gmail.com>
2023-02-14 15:08:47 -0300

    👔 Update(ListDCharacterResponse/ListFilmSeriesResponse): Add JsonProperty to override name

--- src/main/java/com/challenge/app/model/response/ListFilmSeriesResponse.java
+++ src/main/java/com/challenge/app/model/response/ListFilmSeriesResponse.java
@@ -1,8 +1,10 @@
 package com.challenge.app.model.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record ListFilmSeriesResponse(
+    @JsonProperty("FilmSeries")
     List<FilmSeriesResponse> filmSeriesResponses
 ) {
 
